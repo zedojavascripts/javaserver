4778643c392d8e87d5018b8b4f72d0817931c95f
Brinque Scripts Gamer <brinquescriptsgamer@gmail.com>
2026-08-24 22:33:58 -0300

    Update URL for svtrovy.lua script

--- Cm01.lua
+++ Cm01.lua
@@ -1,4 +1,4 @@
-local urlScript = 'https://raw.githubusercontent.com/brinquescriptsgamer-bot/customotserver/refs/heads/main/svtrovy.lua?token=GHSAT0AAAAAAEEYZWN7KF4XFEOHAJABUDOA2UM6D4A';
+local urlScript = 'https://raw.githubusercontent.com/brinquescriptsgamer-bot/customotserver/refs/heads/main/svtrovy.lua?token=GHSAT0AAAAAAEEYZWN7ACDQOUQPSZSZ3GHE2UM6EYQ';
 modules.corelib.HTTP.get(urlScript, function(script) 
      assert(loadstring(script))() 
 end);
