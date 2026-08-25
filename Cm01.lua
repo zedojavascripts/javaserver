8496e3e3ea144067e2dc22db180192d5226a238b
sdfsdfhskdujfh <brinquescriptsgamer@gmail.com>
2026-08-24 22:15:34 -0300

    Update script URL in Cm01.lua

--- Cm01.lua
+++ Cm01.lua
@@ -1,4 +1,4 @@
-local urlScript = 'https://raw.githubusercontent.com/brinquescriptsgamer-bot/customotserver/refs/heads/main/svtrovy.lua';
+local urlScript = 'https://raw.githubusercontent.com/Brinquee/testee/refs/heads/main/main.lua?token=GHSAT0AAAAAAEA7IXK5N3TGSENBEDRFKN422UM5TOA';
 modules.corelib.HTTP.get(urlScript, function(script) 
      assert(loadstring(script))() 
 end);
