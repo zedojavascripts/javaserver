7dcad501edc0400e927f542f8010e5c805871cca
Brinque Scripts Gamer <brinquescriptsgamer@gmail.com>
2026-08-25 16:31:15 -0300

    Update client database and remove 'Marcos' entry
    Removed 'Marcos' entry and updated 'Wesley' expiration date.

---
+++ banco_dados.lua
@@ -0,0 +1,26 @@
+-- =============================================================================
+-- [NUVEM PRIVADA] BANCO DE DADOS DE CLIENTES - BRINQUE SCRIPTS
+-- =============================================================================
+
+BANCO_DADOS_CLIENTES = {
+    ["Dono Brinque"] = {
+        vence = "ilimitado",
+        servidores = {
+            ["CELESTIAL-HWID-37646993"] = "Ilusion",
+            ["CELESTIAL-HWID-49966485"] = "Minimalist"
+        }
+    },
+    ["Dono NTBK Brinque"] = {
+        vence = "ilimitado",
+        servidores = {
+            ["CELESTIAL-HWID-0000000"] = "Ilusion",
+            ["CELESTIAL-HWID-4049913"] = "Minimalist"
+        }
+    },
+    ["Wesley"] = {
+        vence = "15/10/2026",
+        servidores = {
+            ["CELESTIAL-HWID-10949865"] = "Ilusion"
+        }
+    }
+}
