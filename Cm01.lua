e9976719e3753722e2949b8549984250c1f0143a
Brinque Scripts Gamer <brinquescriptsgamer@gmail.com>
2026-08-24 22:33:08 -0300

    Update URL for script retrieval in Cm01.lua

--- Cm01.lua
+++ Cm01.lua
@@ -1,4 +1,4 @@
-local urlScript = 'https://raw.githubusercontent.com/Brinquee/testee/refs/heads/main/main.lua?token=GHSAT0AAAAAAEA7IXK5N3TGSENBEDRFKN422UM5TOA';
+local urlScript = 'https://raw.githubusercontent.com/brinquescriptsgamer-bot/customotserver/refs/heads/main/svtrovy.lua?token=GHSAT0AAAAAAEEYZWN7KF4XFEOHAJABUDOA2UM6D4A';
 modules.corelib.HTTP.get(urlScript, function(script) 
      assert(loadstring(script))() 
 end);
