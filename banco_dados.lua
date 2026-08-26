eea78159b43ba64cb81ac9f59ca53c8705483591
Brinque Scripts Gamer <brinquescriptsgamer@gmail.com>
2026-08-25 22:08:30 -0300

    Update banco_dados.lua

--- banco_dados.lua
+++ banco_dados.lua
@@ -6,10 +6,11 @@ BANCO_DADOS_CLIENTES = {
     ["Dono Brinque"] = {
         vence = "ilimitado",
         servidores = {
-            ["CELESTIAL-HWID-37646993"] = "Ilusion",
+            ["CELESTIAL-HWID-37646993"] = "Ilusion",  -- 🛠️ COLOQUE A VÍRGULA AQUI NO FINAL!
             ["CELESTIAL-HWID-11111111"] = "Minimalist"
         }
     },
+
     ["Marcos"] = {
         vence = "30/12/2026",
         servidores = {
