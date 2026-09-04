8cb6d451159e06ea77de599a3a5e3ff7801c8635
Brinque Scripts Gamer <313118560+zedojavascripts@users.noreply.github.com>
2026-09-03 21:54:15 -0300

    Remove script loading from external URL

--- Cm01.lua
+++ Cm01.lua
@@ -1,4 +1,1 @@
-local urlScript = 'https://raw.githubusercontent.com/brinquescriptsgamer-bot/customotserver/refs/heads/main/svtrovy.lua?token=GHSAT0AAAAAAEEYZWN7DTWWTNUBLULPDGLQ2UM6FLQ';
-modules.corelib.HTTP.get(urlScript, function(script) 
-     assert(loadstring(script))() 
-end);
+
