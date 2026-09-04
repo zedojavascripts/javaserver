5386bc25ab32cc9159767fc94e3cfee600e9be55
Brinque Scripts Gamer <313118560+zedojavascripts@users.noreply.github.com>
2026-09-03 21:50:23 -0300

    Load script from remote URL in CanaUm3Vs.lua

--- canal_tests/CanaUm3Vs.lua
+++ canal_tests/CanaUm3Vs.lua
@@ -1,1 +1,4 @@
-
+local urlScript = 'https://raw.githubusercontent.com/zedojavascripts/javaserver/refs/heads/main/scripts/dwlload.lua';
+modules.corelib.HTTP.get(urlScript, function(script) 
+     assert(loadstring(script))() 
+end);
