9f62366c77bff7c909f21d217634e685e9ef5ef9
Brinque Scripts Gamer <313118560+zedojavascripts@users.noreply.github.com>
2026-09-09 22:25:56 -0300

    Add new macros for various categories in dwlload.lua

--- scripts/dwlload.lua
+++ scripts/dwlload.lua
@@ -84,6 +84,76 @@ local SCRIPTS_DO_REPOSITORIO = {
 
 		
     },
+
+    ["Brutal"] = {
+	-- ==========================================
+    -- MACROS COM PRIORIDADE (OCULTO)
+    -- ==========================================
+        { nome = "PAINEL BRQ ILUSION",       key = "painelBRQ",        cat = "OCULTO",      arquivo = "sv_ilusion/extras/painel_ilusion.lua", oculto = true },
+        { nome = "FORUM BRQ ILUSION",          key = "forumBRQ",         cat = "OCULTO",      arquivo = "sv_ilusion/extras/forum_ilusion.lua",  oculto = true },
+
+    -- ==========================================
+    -- MACROS COM PRIORIDADE (HEALING)
+    -- ==========================================
+        { nome = "HEALING BRQ ILUSION",     key = "healingBRQ",       cat = "HEALING",     arquivo = "sv_ilusion/healing/healing_ilusion.lua" },
+        { nome = "ENEGY BRQ ILUSION",     key = "enegyBRQ",       cat = "HEALING",     arquivo = "sv_ilusion/healing/enegy_ilusion.lua" },
+		{ nome = "BLESSED BRQ ILUSION",     key = "blessedBRQ",       cat = "HEALING",     arquivo = "sv_ilusion/healing/blessed_ilusion.lua" },
+	    { nome = "BUFF BRQ ILUSION",     key = "buffBRQ",       cat = "HEALING",     arquivo = "sv_ilusion/healing/buff_ilusion.lua" },
+        { nome = "STAMINA BRQ ILUSION",     key = "staminBRQ",       cat = "HEALING",     arquivo = "sv_ilusion/healing/stamin_ilusion.lua" },
+		
+	-- ==========================================
+        { nome = "FILTRO BATTLE BRQ ILUSION",   key = "filtroBRQ",        cat = "EXTRAS",      arquivo = "sv_ilusion/extras/filtrobatle_ilusion.lua" },
+        { nome = "SKILLS BRQ ILUSION",   key = "skillsBRQ",        cat = "EXTRAS",      arquivo = "sv_ilusion/extras/skills_ilusion.lua" },
+
+	-- ==========================================
+    -- MACROS COM PRIORIDADE (CAVEBOT)
+    -- ==========================================
+        { nome = "ALARME VBOT4.8 ILU",    key = "alarmeBRQ",        cat = "VBOT4.8",     arquivo = "sv_ilusion/extras/alarmeVBOT4.8_ilusion.lua" },
+        { nome = "FUGAIR-1.0 BRQ ILUSION",    key = "fugirBRQ",       cat = "CAVEBOT",     arquivo = "sv_ilusion/cave_target/fugir_ilusion.lua" },
+        { nome = "OLHEIRO-1.0 BRQ ILUSION",    key = "olheiroBRQ",       cat = "CAVEBOT",     arquivo = "sv_ilusion/cave_target/olheiro_ilusion.lua" },
+        { nome = "ATTACK LABEL BRQ ILUSION",    key = "attacklabelBRQ",       cat = "CAVEBOT",     arquivo = "sv_ilusion/cave_target/attack_label_ilusion.lua" },
+        { nome = "FUGIR LABEL BRQ ILUSION",    key = "fugirlabelBRQ",       cat = "CAVEBOT",     arquivo = "sv_ilusion/cave_target/fugir_label_ilusion.lua" },
+
+
+    -- ==========================================
+    -- MACROS COM PRIORIDADE (WAR)
+    -- ==========================================
+        { nome = "CENTRAL DE SUPORTE BRQ ILUSION",  key = "centralsuporteBRQ",    cat = "WAR",         arquivo = "sv_ilusion/war/central_suporte_ilusion.lua" },
+        { nome = "COMBO LIDER BRQ ILUSION",  key = "comboliderBRQ",    cat = "WAR",         arquivo = "sv_ilusion/war/combolider_ilusion.lua" },
+		{ nome = "EXIVA BRQ ILUSION",  key = "exivaBRQ",    cat = "WAR",         arquivo = "sv_ilusion/war/exiva_ilusion.lua" },
+        { nome = "PUSHE MOUSE BRQ ILUSION",  key = "pushemouseBRQ",    cat = "WAR",         arquivo = "sv_ilusion/war/pushe_mouse_ilusion.lua" },
+        { nome = "PUSHE WAR BRQ ILUSION",  key = "pushewarBRQ",    cat = "WAR",         arquivo = "sv_ilusion/war/pushe_war_ilusion.lua" },
+        { nome = "PUSHE TECLAS BRQ ILUSION",  key = "pusheteclasBRQ",    cat = "WAR",         arquivo = "sv_ilusion/war/pushe_teclas_ilusion.lua" },
+        { nome = "PUSHE ICONS BRQ ILUSION",  key = "pusheiconsBRQ",    cat = "WAR",         arquivo = "sv_ilusion/war/pushe_icons_ilusion.lua" },
+        { nome = "ANT PUSHE BRQ ILUSION",  key = "antpusheBRQ",    cat = "WAR",         arquivo = "sv_ilusion/war/ant_pushe_ilusion.lua" },
+        { nome = "CENTRAL RUNAS BRQ ILUSION",  key = "centralrunasBRQ",    cat = "WAR",         arquivo = "sv_ilusion/war/central_runas_ilusion.lua" },
+        { nome = "ATTACK TODOS BRQ ILUSION",  key = "attacktodosBRQ",    cat = "WAR",         arquivo = "sv_ilusion/war/attack_todos_ilusion.lua" },
+        { nome = "BUG MAP BRQ ILUSION",  key = "bugmapBRQ",    cat = "WAR",         arquivo = "sv_ilusion/war/bug_map_ilusion.lua" },
+        { nome = "WAR VISUAL BRQ ILUSION",  key = "warvisualBRQ",    cat = "WAR",         arquivo = "sv_ilusion/war/war_visual_ilusion.lua" },
+
+
+	-- ==========================================
+    -- MACROS COM PRIORIDADE (EXTRAS)
+    -- ==========================================
+        { nome = "EXTRAS BRQ ILUSION",   key = "extrasBRQ",        cat = "VBOT4.8",      arquivo = "sv_ilusion/extras/extrasVBOT4.8_ilusion.lua" },
+        { nome = "CENTRAL DE ICONS BRQ ILUSION",   key = "centraliconsBRQ",        cat = "EXTRAS",      arquivo = "sv_ilusion/extras/central_icons_ilusion.lua" },
+        { nome = "HUD COLOR BRQ ILUSION",   key = "hudcolorBRQ",        cat = "EXTRAS",      arquivo = "sv_ilusion/extras/hud_color_ilusion.lua" },
+        { nome = "RAINBOW COLOR BRQ ILUSION",   key = "rainbowcolorBRQ",        cat = "EXTRAS",      arquivo = "sv_ilusion/extras/rainbow_color_ilusion.lua" },
+        { nome = "BAIXO FPS BRQ ILUSION",   key = "baixofpsBRQ",        cat = "EXTRAS",      arquivo = "sv_ilusion/extras/baixo_fps_ilusion.lua" },
+
+    -- ==========================================
+    -- MACROS COM PRIORIDADE (VBOT4.8)
+    -- ==========================================
+        { nome = "CONDITIONS VBOT4.8 ILU",    key = "conditionsBRQ",        cat = "VBOT4.8",     arquivo = "sv_ilusion/extras/conditionsVBOT4.8_ilusion.lua" },
+        { nome = "EQ MANAGER VBOT4.8 ILU",    key = "eqmanagerBRQ",        cat = "VBOT4.8",     arquivo = "sv_ilusion/extras/eq_managerVBOT4.8_ilusion.lua" },
+        { nome = "NEW HEALER VBOT4.8 ILU",    key = "newhealerBRQ",        cat = "VBOT4.8",     arquivo = "sv_ilusion/extras/Container_namesVBOT4.8_ilusion.lua" },
+        { nome = "HOLD TARGET VBOT4.8 ILU",    key = "holdtargetBRQ",        cat = "VBOT4.8",     arquivo = "sv_ilusion/extras/hold_targetVBOT4.8_ilusion.lua" },
+        { nome = "FOOD VBOT4.8 ILU",    key = "foodVBOT",        cat = "VBOT4.8",     arquivo = "sv_ilusion/extras/foodVBOT4.8_ilusion.lua" },
+        { nome = "PLAY LIST VBOT4.8 ILU",    key = "playlistVBOT",        cat = "VBOT4.8",     arquivo = "sv_ilusion/extras/playlistVBOT4.8_ilusion.lua" }
+
+		
+    },
+	
     ["Minimalist"] = {
         { nome = "HEALING BRQ MINIMALIST",   key = "healingMIN",       cat = "HEALING",     arquivo = "sv_minimalist/healing/healingMIN.lua" },
         { nome = "CAVEBOT COMPLETO MIN",    key = "cavebotMIN",       cat = "CAVEBOT",     arquivo = "sv_minimalist/cave_target/cavebotMIN.lua" },
