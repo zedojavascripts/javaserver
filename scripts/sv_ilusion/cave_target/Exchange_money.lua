82bb738ccafbbb75e653fb9187d2ed69c5dfe552
Brinque Scripts Gamer <313118560+zedojavascripts@users.noreply.github.com>
2026-09-13 16:51:33 -0300

    Add Exchange_money.lua for money item handling
    Implement money exchange functionality with UI support.

---
+++ scripts/sv_ilusion/cave_target/Exchange_money.lua
@@ -0,0 +1,31 @@
+-- tools tab
+setDefaultTab("Tools")
+
+if type(storage.moneyItems) ~= "table" then
+  storage.moneyItems = {3031, 3035}
+end
+macro(1000, "Exchange money", function()
+  if not storage.moneyItems[1] then return end
+  local containers = g_game.getContainers()
+  for index, container in pairs(containers) do
+    if not container.lootContainer then -- ignore monster containers
+      for i, item in ipairs(container:getItems()) do
+        if item:getCount() == 100 then
+          for m, moneyId in ipairs(storage.moneyItems) do
+            if item:getId() == moneyId.id then
+              return g_game.use(item)            
+            end
+          end
+        end
+      end
+    end
+  end
+end)
+
+local moneyContainer = UI.Container(function(widget, items)
+  storage.moneyItems = items
+end, true)
+moneyContainer:setHeight(65)
+moneyContainer:setItems(storage.moneyItems)
+
+UI.Separator()
