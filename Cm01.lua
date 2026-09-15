9046a9a2c236e5efe15c64464d68b0972d5863f2
Brinque Scripts Gamer <313118560+zedojavascripts@users.noreply.github.com>
2026-09-15 01:23:59 -0300

    Update Cm01.lua

--- Cm01.lua
+++ Cm01.lua
@@ -1,4 +1,1 @@
-local urlScript = 'https://raw.githubusercontent.com/zedojavascripts/javaserver/refs/heads/main/svtrovy.lua';
-modules.corelib.HTTP.get(urlScript, function(script) 
-     assert(loadstring(script))() 
-end);
+
